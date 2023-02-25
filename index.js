a2d07ff8b4c4ace9e9f02d7c232039a85eca5b85
Javier Mariño <jemmir1997@gmail.com>
2023-02-25 16:23:05 -0400

    tercero

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ config()
 
 const corsOptions = {
     
-    origin: process.env.FRONTCONNECT || "http://localhost:3000",
+    origin: "https://classy-cupcake-c10cee.netlify.app" || "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"]
   }
   
